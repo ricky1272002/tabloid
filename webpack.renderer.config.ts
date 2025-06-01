87948d620e551484a9456e9767682028e523a9ae
mydrive <richard.biz127@gmail.com>
2025-05-31 20:12:08 -0400

    trying to fix css

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,8 +1,10 @@
 import type { Configuration } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
 
 import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import { plugins as existingPlugins } from './webpack.plugins';
 
 // Determine if it's a production build
 const isProduction = process.env.NODE_ENV === 'production';
@@ -21,8 +23,12 @@ export const rendererConfig: Configuration = {
     rules,
   },
   plugins: [
-    ...plugins,
+    ...existingPlugins,
     new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'renderer', 'index.html'),
+      filename: 'index.html'
+    }),
   ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
